0d711633084b212f47dff997889c7e7afa936d19
AnyaSonina <89742951+AnyaSonina@users.noreply.github.com>
2021-11-18 15:04:23 +0000

    event listener is added

--- js/index.js
+++ js/index.js
@@ -1,1 +1,7 @@
-document.getElementById('count').innerText=5
+let countEl = document.getElementById("count-el");
+let count = 0;
+
+function increment() {
+  count = count + 1;
+  countEl.innerText = count;
+}
