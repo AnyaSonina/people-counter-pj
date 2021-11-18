ea89fdeae2a54dd7217a87fb42891017a0cd7a34
AnyaSonina <89742951+AnyaSonina@users.noreply.github.com>
2021-11-18 16:49:00 +0000

    new element and its java script is added

--- js/index.js
+++ js/index.js
@@ -1,12 +1,15 @@
+let saveEl = document.getElementById("save-el");
 let countEl = document.getElementById("count-el");
 let count = 0;
 
 function increment() {
-  count = count + 1;
+  count += 1;
   countEl.innerText = count;
 }
 
 function save() {
+  let showedNumber = count + " -";
+  saveEl.innerText += " " + showedNumber;
   console.log(count)
 }
 
